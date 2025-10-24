dc9fe886ce954b7299332896d9be496d72e823b3
dhufoby <gbblue123@gmail.com>
2025-10-24 16:59:02 +0900

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,15 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is no longer used.
+// All application code has been consolidated into index.html
+// to fix file loading issues on GitHub Pages.
+// You can safely delete this file.
